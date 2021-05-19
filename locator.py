c9bd1c3ae5b138bd1a9f886f6914d1f299fcc1aa
Kevin Johnson <52178579+kj730@users.noreply.github.com>
2021-05-19 09:46:03 -0700

    Update locator.py
    Finished first part of section. Code searches for and returns the searched for item that was most recent to the list.

--- locator.py
+++ locator.py
@@ -1,11 +1,14 @@
-import os
-import time
 import sys
+
 THD_COL = 4
 YEAR_COL = 7
 MONTH_COL = 8
-thd_Arr = []
+
+
+
 def locateFunction(year, month, strike, callPut):
+    answer = ""
+    thd_Arr = []
     try:
         general = open("FBQNGM1.General.20210503.log", "r")
     except OSError as err:
@@ -15,28 +18,43 @@ def locateFunction(year, month, strike, callPut):
         print("Unable to open General File")
         return -1
     for line in general:
-        split = line.split('|')
-        thdStr = split[THD_COL]
-        if thdStr == "THD":
+        splitter = line.split('|')
+        if len(splitter) < THD_COL + 1:
+            continue
+        thdStr = splitter[THD_COL]
+        if thdStr == " THD ":
             thd_Arr.append(line)
     for x in thd_Arr:
-        splitter = x.split('|')
-        yearStr = splitter[YEAR_COL]
-        monthStr = splitter[MONTH_COL]
+        seperate = x.split('|')
+        yearStr = seperate[YEAR_COL]
+        monthStr = seperate[MONTH_COL]
     thd_Arr.reverse()
     if callPut == "call":
+        found = False
         for x in thd_Arr:
             cutter = x.split('|')
+            result = x.find("C:3230:")
             for y in cutter:
-               if y[0:6] == "C:" + strike:
-                   print(y)
+                if y[0:7] == "C:" + strike + ":":
+                    answer = y
+                    found = True
+                    break
+            if found:
+                break
     elif callPut == "put":
+        found = False
         for x in thd_Arr:
             split = x.split('|')
             for y in split:
-               if y[0:6] == "P:" + strike:
-                   print(y)
+                if y[0:6] == "P:" + strike + ":":
+                    answer = y
+                    found = True
+                    break
+            if found:
+                break
     general.close()
+    return answer
+
 def main():
     if len(sys.argv) < 5:
         print("Usage: ", sys.argv[0], "Year Month Strike Call/Put")
@@ -46,4 +64,6 @@ def main():
     strike = sys.argv[3]
     callPut = sys.argv[4]
     answer = locateFunction(year, month, strike, callPut)
-    exit(answer)
+    print(answer)
+if __name__ == "__main__":
+    main()
