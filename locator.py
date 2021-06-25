05181abf893299cf5d9fa86b6c343412bf683a3e
Kevin Johnson <kevinjohnson@kj-macbook-pro.lan>
2021-06-25 12:41:46 -0500

    Added output value names. Added output test case for empty data. Added time output.

--- locator.py
+++ locator.py
@@ -68,24 +68,27 @@ def locateFunction(filename, year, month, strike, callPut, time_input):
 #formarts the data to see each value in an easier way
 def format_output(data):
     arr = data.split(':')
-    print("Kind=" + arr[OUTPUT_KIND_IDX])
-    print("Strike=" + arr[OUTPUT_STK_IDX])
-    print("Value3=" + arr[2])
-    print("Value4=" + arr[3])
-    print("Value5=" + arr[4])
-    print("Value6=" + arr[5])
-    print("Value7=" + arr[6])
-    print("Value8=" + arr[7])
-    print("Value9=" + arr[8])
-    print("Value10=" + arr[9])
-    print("Value11=" + arr[10])
-    print("Value12=" + arr[11])
-    print("Value13=" + arr[12])
-    print("Value14=" + arr[13])
+    if(len(data) == 0):
+        print("Data not found for year = ", sys.argv[2], " month = ", sys.argv[3], " strike = ", sys.argv[4], " Call/Put = ", sys.argv[5], "time = ", sys.argv[6])
+    else:
+        print("Kind =" + arr[OUTPUT_KIND_IDX])
+        print("Strike =" + arr[OUTPUT_STK_IDX])
+        print("Value3 =" + arr[2])
+        print("BidIn =" + arr[3])
+        print("BidOut =" + arr[4])
+        print("BidQty =" + arr[5])
+        print("On/Off =" + arr[6])
+        print("AskIn =" + arr[7])
+        print("AskOut =" + arr[8])
+        print("AskQty =" + arr[9])
+        print("Value11=" + arr[10])
+        print("Value12=" + arr[11])
+        print("Value13=" + arr[12])
+        print("Value14=" + arr[13])
 
 def main():
     if len(sys.argv) < 6:
-        print("Usage: ", sys.argv[0], "Filename Year Month Strike Call/Put Time")
+        print("Usage: ", sys.argv[0], "Filename Year Month Strike Call/Put(C or P) Time")
         exit(-1)
     filename = sys.argv[1]
     year = sys.argv[2]
@@ -98,6 +101,7 @@ def main():
     answer = locateFunction(filename, year, month, strike, callPut, time_input)
     print("Answer is " + answer)
     if len(answer) > 3:
+        print("Time: ", date_time_obj)
         format_output(answer)
     else:
         print("data not found")
