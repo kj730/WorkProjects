7f66d1d165643914652c2cd42b548bb8fc64028f
Kevin Johnson <kevinjohnson@kj-macbook-pro.lan>
2021-06-25 13:41:27 -0500

    Fixed empty data entry output

--- locator.py
+++ locator.py
@@ -21,8 +21,11 @@ def locateFunction(filename, year, month, strike, callPut, time_input):
     except:
         print("Unable to open General File")
         return -1
+    
     for line in general:
         splitter = line.split('|')
+        if len(splitter) == 0:
+            return("Data not found for year = ", sys.argv[2], " month = ", sys.argv[3], " strike = ", sys.argv[4], " Call/Put = ", sys.argv[5], "time = ", sys.argv[6])
         #check to see if we are using the right line in the file
         if len(splitter) < THD_COL + 1:
             continue
@@ -68,23 +71,20 @@ def locateFunction(filename, year, month, strike, callPut, time_input):
 #formarts the data to see each value in an easier way
 def format_output(data):
     arr = data.split(':')
-    if(len(data) == 0):
-        print("Data not found for year = ", sys.argv[2], " month = ", sys.argv[3], " strike = ", sys.argv[4], " Call/Put = ", sys.argv[5], "time = ", sys.argv[6])
-    else:
-        print("Kind =" + arr[OUTPUT_KIND_IDX])
-        print("Strike =" + arr[OUTPUT_STK_IDX])
-        print("Value3 =" + arr[2])
-        print("BidIn =" + arr[3])
-        print("BidOut =" + arr[4])
-        print("BidQty =" + arr[5])
-        print("On/Off =" + arr[6])
-        print("AskIn =" + arr[7])
-        print("AskOut =" + arr[8])
-        print("AskQty =" + arr[9])
-        print("Value11=" + arr[10])
-        print("Value12=" + arr[11])
-        print("Value13=" + arr[12])
-        print("Value14=" + arr[13])
+    print("Kind =" + arr[OUTPUT_KIND_IDX])
+    print("Strike =" + arr[OUTPUT_STK_IDX])
+    print("Value3 =" + arr[2])
+    print("BidIn =" + arr[3])
+    print("BidOut =" + arr[4])
+    print("BidQty =" + arr[5])
+    print("On/Off =" + arr[6])
+    print("AskIn =" + arr[7])
+    print("AskOut =" + arr[8])
+    print("AskQty =" + arr[9])
+    print("Value11=" + arr[10])
+    print("Value12=" + arr[11])
+    print("Value13=" + arr[12])
+    print("Value14=" + arr[13])
 
 def main():
     if len(sys.argv) < 6:
@@ -99,8 +99,9 @@ def main():
     #make a time object from the time argument
     date_time_obj = datetime.datetime.strptime(time_input, '%H:%M:%S')
     answer = locateFunction(filename, year, month, strike, callPut, time_input)
-    print("Answer is " + answer)
-    if len(answer) > 3:
+    if isinstance(answer, str) == True:
+        print(answer)
+    elif len(answer) > 3:
         print("Time: ", date_time_obj)
         format_output(answer)
     else:
